4d374de38e57adce744980b5fd9f192f518f69bd
William Kwan <William Kwan>
2016-11-18 06:22:28 -0500

    s

--- flaskapp.py
+++ flaskapp.py
@@ -19,5 +19,6 @@ def gen():
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
+    os.environ["KERAS_BACKEND"] = "theano"
     print("port is", port);
     app.run(host='0.0.0.0', port=port)
