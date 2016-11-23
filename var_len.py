8dd4b58495f652f554e087d5e8b0b90e17c695b1
William Kwan <William Kwan>
2016-11-23 08:24:58 -0500

    new data

--- var_len.py
+++ var_len.py
@@ -19,8 +19,8 @@ with open('int_to_card.pickle', 'rb') as handle:
 with open('model.json', 'r') as handle:
     loaded_model_json = handle.read()
 model = model_from_json(loaded_model_json)
+model.load_weights("weightsmoredecks.final.h5")
 # model.load_weights("weights.final.h5")
-model.load_weights("weights.final.h5")
 
 
 def train():
@@ -69,7 +69,7 @@ def train():
     for deck in decks:
         deck_ints = list(card_to_int[card] for index, card in enumerate(deck))
         print("Deck is", deck_ints)
-        for i in range(50):
+        for i in range(1000):
             input_len = numpy.random.randint(2, MAX_LEN)
             input = numpy.random.choice(deck_ints, input_len)
             dataX.append(input[:len(input) - 1])
@@ -90,32 +90,30 @@ def train():
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
 
-    # filepath = "weights.best.h5"
-    # filepath = "weights.testbest.h5"
-    # checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    # callbacks_list = [checkpoint]
+    filepath = "weightsmoredecks.best.h5"
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    callbacks_list = [checkpoint]
 
-    # model = create_model()
-    # model.fit(X, y, nb_epoch=100, batch_size=1, verbose=2, validation_split=0.33, callbacks=callbacks_list)
-    # model_json = model.to_json()
-    # with open("model.json", "w") as json_file:
-    #     json_file.write(model_json)
-    # model.save_weights("weights.testfinal.h5")
-    # scores = model.evaluate(X, y, verbose=0)
-    # print("Model Accuracy: %.2f%%" % (scores[1] * 100))
+    model = create_model()
+    model.fit(X, y, nb_epoch=50, batch_size=100, verbose=1, validation_split=0.33, callbacks=callbacks_list)
+    model_json = model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights("weightsmoredecks.final.h5")
+    scores = model.evaluate(X, y, verbose=0)
+    print("Model Accuracy: %.2f%%" % (scores[1] * 100))
 
-    model = KerasClassifier(build_fn=create_model)
-
-
-    nb_epochs = [50, 100, 200, 500]
-    batch_sizes = [1, 5, 10, 20, 30]
-    param_grid = dict(nb_epoch=nb_epochs, batch_size=batch_sizes, verbose=[2], validation_split=[0.33])
-    grid = GridSearchCV(estimator=model, param_grid=param_grid)
-    grid_result = grid.fit(X, y)
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-
-    for params, mean_score, scores in grid_result.grid_scores_:
-        print("%f (%f) with: %r" % (scores.mean(), scores.std(), params))
+    # model = KerasClassifier(build_fn=create_model)
+    #
+    # nb_epochs = [100, 125, 150]
+    # batch_sizes = [1000]
+    # param_grid = dict(nb_epoch=nb_epochs, batch_size=batch_sizes, verbose=[1], validation_split=[0.33])
+    # grid = GridSearchCV(estimator=model, param_grid=param_grid)
+    # grid_result = grid.fit(X, y)
+    # print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    #
+    # for params, mean_score, scores in grid_result.grid_scores_:
+    #     print("%f (%f) with: %r" % (scores.mean(), scores.std(), params))
 
 
     return model
@@ -134,6 +132,7 @@ def generate_deck(seed_cards):
         padded_input = pad_sequences([seed_input], maxlen=MAX_INPUT_LEN)
         padded_input = padded_input / float(len(card_to_int))
         prediction = model.predict(padded_input, verbose=0)
+        print("prediction", prediction)
         index = numpy.argmax(prediction)
         seed_input.append(index)
 
@@ -141,11 +140,12 @@ def generate_deck(seed_cards):
 
 
 if __name__ == "__main__":
-    test_input_text = ["Savage Roar", "Living Roots", "Swipe"]
+    # test_input_text = ["Flame Imp", "SoulFire", "Voidwalker", "Dark Peddler", "Wrathguard", "Imp Gang Boss"]
+    # test_input_text = ["Savage Roar", "Living Roots", "Swipe"]
     # test_input_text = ["Earthen Ring Farseer", "Argent Squire", "Bloodmage Thalnos"]
     # test_input_text = ["Mounted Raptor LoE", "Mad Scientist Naxx", "Alexstrasza"]
     # test_input_text = ["Ice Barrier", "Frostbolt", "Archmage Antonidas", "Spider Tank GvG", "Loatheb Naxx", "Annoy-o-Tron GvG", "Cogmaster GvG"]
-    # test_input_text = ["Northshire Cleric", "Twilight Guardian TGT", "Sylvanas Windrunner", "Dr. Boom", "Wild Pyromancer"]
+    test_input_text = ["Northshire Cleric", "Twilight Guardian TGT", "Sylvanas Windrunner", "Dr. Boom", "Wild Pyromancer"]
     # test_input_text = ["Cat Trick", "Unleash the Hounds", "Call of the Wild"]
-    # print(generate_deck(test_input_text))
-    model = train()
+    print(generate_deck(test_input_text))
+    # model = train()
